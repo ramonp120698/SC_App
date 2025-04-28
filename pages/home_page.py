e7773a5bc5a9ac448487d24d209fc93e2db1b16c
ramonp120698 <ramonp.128@gmail.com>
2025-04-28 00:36:15 -0700

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,4 +1,15 @@
 import streamlit as st
 st.header('Home')
 
-st.image("assets/sc_logo.png", width=200)
+# Centrar el contenido
+st.markdown("<h1 style='text-align: center;'>Home</h1>", unsafe_allow_html=True)
+
+# Centrar la imagen y ajustar el tamaño
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center;">
+        <img src="assets/sc_logo.png" width="400">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
