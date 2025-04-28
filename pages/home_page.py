f89ccd46c899fc412adc4f5b65d9b254d3e49b16
ramonp120698 <ramonp.128@gmail.com>
2025-04-28 00:44:08 -0700

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -20,7 +20,7 @@ st.markdown(
         display: block;
         margin-left: auto;
         margin-right: auto;
-        padding-left: 50px; 
+        padding-left: 150px; 
     }
     </style>
     """,
