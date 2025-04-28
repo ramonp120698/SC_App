262a8c8fa96344f0bc2b1af640870a4c0bbe8837
ramonp120698 <ramonp.128@gmail.com>
2025-04-28 00:14:57 -0700

    Update sc_streamlit_app.py

--- sc_streamlit_app.py
+++ sc_streamlit_app.py
@@ -35,8 +35,8 @@ project_2_page = st.Page(
 # --- NAVIGATION SETUP [WITH SECTIONS]---
 pg = st.navigation(
     {
-        "Info":[home_page],
-        "Projects": [about_page],[project_1_page, project_2_page],
+        [home_page],
+        "Projects": [about_page,project_1_page, project_2_page],
     }
 )
 
